0b30c60a74cedcfcc75e7a3b438437cbf17b67f3
raycaha <caha07ray@gmail.com>
2022-05-10 11:28:19 -0400

    driver updates

--- Drivers/ExpressDriver.ts
+++ Drivers/ExpressDriver.ts
@@ -1,7 +1,30 @@
 import cors from "cors";
 import helmet from "helmet";
 import express from "express";
+import cookieParser from"cookie-parser";
+
+const version = require('../../package.json').version
 
 export class ExpressDriver{
+    public static app = express();
     
+    public static build() {
+        return this.buildDriver();
+    }
+
+    private static buildDriver() {
+        this.app.use(helmet());
+        this.app.use(express.json());
+        this.app.use(cors({ origin: true, credentials: true }))
+        this.app.use(cookieParser());
+        
+        this.initServer();
+        return this.app;
+    }
+
+    private static initServer() {
+        this.app.get("/", (req, res) => {
+            res.json({ message: `Welcome to the Employee-Patient-Management-Service Version ${version}`});
+        });
+    }
 }
