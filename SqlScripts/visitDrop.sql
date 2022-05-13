767e66e8cd692aa7b977c00c3e4b96621ea99a35
AsuraJoe <skyrider245@gmail.com>
2022-05-13 19:32:39 -0400

    remove models folder

--- SqlScripts/visitDrop.sql
+++ SqlScripts/visitDrop.sql
@@ -2,6 +2,6 @@
 DROP TABLE IF EXISTS vitals;
 DROP TABLE IF EXISTS diagnosis;
 -- DROP TABLE IF EXISTS visit_billing;
-DROP TABLE IF EXISTS visit;
+-- DROP TABLE IF EXISTS visit;
 -- DROP TABLE IF EXISTS patient;
 -- DROP TABLE IF EXISTS employee;
--- models/QueueEntry.ts
+++
@@ -1,8 +0,0 @@
-export class QueueEntry {
-    patient_id: number;
-    name: String;
-    visit_date: Date;
-    nurse?: String;
-    doctor?: String;
-    status: String;
-}
